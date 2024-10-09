732a50dc97f503f3cd74817a6383087ac6e7553b
pedroaraujox <pa8088253@gmail.com>
2024-10-08 22:02:26 -0300

    att 5

--- testes.js
+++ testes.js
@@ -4,3 +4,5 @@ torrar("pao de forma")
 function torrar(pao, nome = "Cliente", valor = "5.76"){ //usado para definir um padrao para quando nao tiver uma valor atribu 
     console.log("Essa torrada feita de " + pao + " é do " + nome + " e o valor ficou " + valor)
 }
+
+console.log("Vai ser tao lindo")
