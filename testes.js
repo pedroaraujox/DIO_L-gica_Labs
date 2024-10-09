6cde4b8ccc76f4505651942db15db9ad24030acc
pedroaraujox <pa8088253@gmail.com>
2024-10-08 21:08:01 -0300

    atualizacao 2

--- testes.js
+++ testes.js
@@ -1,8 +1,6 @@
-torrar("pao de forma", "Pedro Araujo")
-torrar("pao de mel", "Sabrina Martins")
+torrar("pao de forma")
 
 
-
-function torrar(pao, nome){
-    console.log("Essa torrada feita de " + pao + " é do " + nome)
+function torrar(pao, nome = "Cliente", valor = "5.76"){ //usado para definir um padrao para quando nao tiver uma valor atribu 
+    console.log("Essa torrada feita de " + pao + " é do " + nome + " e o valor ficou " + valor)
 }
